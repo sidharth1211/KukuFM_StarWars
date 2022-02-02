62d8aa8ba0864477dc8bc7a938c06f64cb48202d
sidharth1211 <communicate.sidharth@gmail.com>
2022-02-02 14:01:14 +0530

    Added Header Navbar and Footer copyright

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-     May the Force be with you
-    </div>
+    <>
+    <Header/>
+    <main>
+      <Container>
+    <h1>May the Force be with you</h1> 
+    </Container>
+    </main>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import  { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container, Row, Col } from 'react-bootstrap'
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+<Container>
+
+<Navbar.Brand href="#home">
+      <img
+        src="/StarWarslogo.png"
+        width="60"
+        height="40"
+        className="d-inline-block align-top"
+      
+      />   
+    </Navbar.Brand>
+
+
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+
+
+    <Nav >
+
+      <Nav.Link href="/people"><i className="fas fa-shopping-cart"></i> People</Nav.Link>
+      <Nav.Link href="/planets"><i className="fas fa-shopping-cart"></i> Planets</Nav.Link>
+      
+
+
+    </Nav>
+
+
+    <Form className="d-flex">
+      <FormControl
+        type="search"
+        placeholder="Search People"
+        className="mr-2"
+        aria-label="Search"
+      />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+
+
+  </Container>
+</Navbar>
+        </header>
+    )
+}
+
+export default Header
